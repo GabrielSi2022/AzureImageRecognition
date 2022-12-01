a6ffa4b50350870a60de52b41bbe914537667f2d
Paulo Gonçalves <paulo.programmator@gmail.com>
2022-12-01 14:18:41 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './Components/Header'
+import Banner from './Components/Banner'
+import './index.css'
+import ComputerVision from './Components/ComputerVision';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div>
+        <Header />
+        <Banner
+          videoTitle={"Meu Video"}
+          url={"https://www.youtube.com/watch?v=3BhkeY974Rg"}
+          videoDescription={"IFSULDEMINAS"}
+          />
+         <ComputerVision />  
+        
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
